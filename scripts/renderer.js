9272fa2597b5c52d22ccde6ce8d26c94e0ec071c
k0rean-rand0m <15913538+k0rean-rand0m@users.noreply.github.com>
2022-10-14 17:00:42 +0800

    HexHeads Bruteforce, HexHeads Scan, Web3 Library Verifier

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,32 +1,31 @@
-
-function render(address) {
-    // const address = (new URLSearchParams(window.location.search)).get("address").slice(2);
-    // 0x3A205E'Cf286'bBe11'46063'8aCe4'7D501'A53fB'91C0
+function buildTraits(address) {
     if (address.length === 42) address = address.slice(2)
-
     const displacement = parseInt(address.substring(36, 40), 16)
-    const ctx = document.getElementById('canvas').getContext('2d');
     const traits = []
-
-    function buildTraits() {
-        traits.push("#"+address.substring(0, 6))
-        for(let i = 6; i < 36; i+=5) {
-            let trait
-            if(i === 6) {
-                trait = (parseInt(address.substring(6, 11), 16) + displacement) % 10
-            } else if (i === 16) {
-                trait = (parseInt(address.substring(16, 21), 16) + displacement) % 12
-            } else {
-                trait = (parseInt(address.substring(i, i+5), 16) + displacement) % 30
-            }
-            traits.push(trait)
-            console.log(trait)
+    traits.push("#"+address.substring(0, 6))
+    for(let i = 6; i < 36; i+=5) {
+        let trait
+        if(i === 6) {
+            trait = (parseInt(address.substring(6, 11), 16) + displacement) % 10
+        } else if (i === 16) {
+            trait = (parseInt(address.substring(16, 21), 16) + displacement) % 12
+        } else {
+            trait = (parseInt(address.substring(i, i+5), 16) + displacement) % 30
         }
+        traits.push(trait)
     }
-    buildTraits()
+    return traits
+}
+
+function renderFromTraits(traits) {
+    const ctx = document.getElementById('canvas').getContext('2d');
 
     function drawBackground() {
-        ctx.fillStyle = traits[0];
+        let color = traits[0];
+        if (color === undefined) {
+            color = "#FFFFFF";
+        }
+        ctx.fillStyle = color;
         ctx.rect(0, 0, 32, 32);
         ctx.fill();
     }
@@ -45,4 +44,10 @@ function render(address) {
         }
     };
     img.src = 'img/HexHeads.png';
+}
+
+function render(address) {
+    // 0x3A205E'Cf286'bBe11'46063'8aCe4'7D501'A53fB'91C0
+    const traits =  buildTraits(address)
+    renderFromTraits(traits)
 }
