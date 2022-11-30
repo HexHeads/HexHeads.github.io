7f4b04dda093cb058f5ed2e158c47e07ff6f084f
k0rean-rand0m <15913538+k0rean-rand0m@users.noreply.github.com>
2022-11-30 13:23:13 +0800

    Foil HexHeads (demo)

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -2,38 +2,54 @@ function buildTraits(address) {
     if (address.length === 42) address = address.slice(2)
     const displacement = parseInt(address.substring(36, 40), 16)
     const traits = []
-    traits.push("#"+address.substring(0, 6))
-    for(let i = 6; i < 36; i+=5) {
+    const foil = bigInt(address, 16).mod(bigInt("100")).compare(0) === 0;
+
+    if (foil) {
+        traits.push("Foil");
+        traits.push("Foil");
+    } else {
+        traits.push("#"+address.substring(0, 6));
+        traits.push((parseInt(address.substring(6, 11), 16) + displacement) % 10);
+    }
+
+    for(let i = 11; i < 36; i+=5) {
         let trait
-        if(i === 6) {
-            trait = (parseInt(address.substring(6, 11), 16) + displacement) % 10
-        } else if (i === 16) {
+        if (i === 16) {
             trait = (parseInt(address.substring(16, 21), 16) + displacement) % 12
         } else {
             trait = (parseInt(address.substring(i, i+5), 16) + displacement) % 30
         }
         traits.push(trait)
     }
+    console.log(traits)
     return traits
 }
 
 function renderFromTraits(traits) {
     const ctx = document.getElementById('canvas').getContext('2d');
+    let color = traits[0];
 
     function drawBackground() {
-        let color = traits[0];
-        if (color === undefined) {
-            color = "#FFFFFF";
+        if(color === "Foil") {
+            ctx.drawImage(img,
+                0, 192, 32, 32,
+                0, 0, 32, 32);
+        } else {
+            if (color === undefined) {
+                color = "#FFFFFF";
+            }
+            ctx.fillStyle = color;
+            ctx.rect(0, 0, 32, 32);
+            ctx.fill();
         }
-        ctx.fillStyle = color;
-        ctx.rect(0, 0, 32, 32);
-        ctx.fill();
     }
 
     function drawTrait(layer, item) {
-        ctx.drawImage(img,
-            item*32, layer*32, 32, 32,
-            0, 0, 32, 32);
+        if (item !== "Foil") {
+            ctx.drawImage(img,
+                item*32, layer*32, 32, 32,
+                0, 0, 32, 32);
+        }
     }
 
     const img = new Image();
@@ -43,11 +59,10 @@ function renderFromTraits(traits) {
             drawTrait(i, traits[i+1]) // 0 is Background
         }
     };
-    img.src = 'img/HexHeads.png';
+    img.src = 'img/HexHeadsFoil.png';
 }
 
 function render(address) {
-    // 0x3A205E'Cf286'bBe11'46063'8aCe4'7D501'A53fB'91C0
     const traits =  buildTraits(address)
     renderFromTraits(traits)
 }
